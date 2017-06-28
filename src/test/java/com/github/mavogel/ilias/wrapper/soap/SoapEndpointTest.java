576b4bf4e844ec5317dff9e417cb27d78861bb42
mavogel <mavogel@posteo.de>
2017-06-28 10:22:27 +0200

    Soap test for get courses for user.

--- src/test/java/com/github/mavogel/ilias/wrapper/soap/SoapEndpointTest.java
+++ src/test/java/com/github/mavogel/ilias/wrapper/soap/SoapEndpointTest.java
@@ -27,25 +27,34 @@ package com.github.mavogel.ilias.wrapper.soap;/*
 import com.github.mavogel.client.ILIASSoapWebserviceBindingStub;
 import com.github.mavogel.client.ILIASSoapWebserviceLocator;
 import com.github.mavogel.client.ILIASSoapWebservicePortType;
+import com.github.mavogel.ilias.model.IliasNode;
 import com.github.mavogel.ilias.model.LoginConfiguration;
 import com.github.mavogel.ilias.wrapper.AbstractIliasEndpoint;
+import com.github.mavogel.ilias.wrapper.DisplayStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mavogel on 6/26/17.
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({SoapEndpoint.class, AbstractIliasEndpoint.class,
-        ILIASSoapWebservicePortType.class, ILIASSoapWebserviceLocator.class})
+        ILIASSoapWebservicePortType.class, ILIASSoapWebserviceLocator.class,
+        SoapXMLUtils.class})
 public class SoapEndpointTest {
 
+    private static final String SID = "sid-123";
+    public static final int USER_ID = 5678;
     private SoapEndpoint classUnderTest;
     private ILIASSoapWebservicePortType endPointMock;
 
@@ -62,8 +71,8 @@ public class SoapEndpointTest {
         PowerMockito.when(locatorMock.getILIASSoapWebservicePort()).thenReturn(endPointMock);
 
         PowerMockito.when(endPointMock.loginLDAP(loginConfiguration.getClient(), loginConfiguration.getUsername(), loginConfiguration.getPassword()))
-                .thenReturn("sid-123");
-        PowerMockito.when(endPointMock.getUserIdBySid("sid-123")).thenReturn(5678);
+                .thenReturn(SID);
+        PowerMockito.when(endPointMock.getUserIdBySid(SID)).thenReturn(USER_ID);
 
         // == set
         classUnderTest = new SoapEndpoint(loginConfiguration);
@@ -71,13 +80,47 @@ public class SoapEndpointTest {
 
     @Test
     public void shouldLogout() throws Exception {
-        // == go
-        PowerMockito.when(endPointMock.logout("sid-123")).thenReturn(true);
+        // == train
+        PowerMockito.when(endPointMock.logout(SID)).thenReturn(true);
 
         // == go
         classUnderTest.logout();
 
         // == verify
-        Mockito.verify(endPointMock).logout("sid-123");
+        Mockito.verify(endPointMock).logout(SID);
     }
+
+    @Test
+    public void shouldGetCoursesForUser() throws Exception {
+        // == prepare
+        String foundCourses = "foundCourses";
+        List<Integer> courseRefIds = Arrays.asList(1, 2, 3);
+
+        // == train
+        PowerMockito.when(endPointMock.getCoursesForUser(Mockito.eq(SID), Mockito.eq("courseXML")))
+                .thenReturn(foundCourses);
+        PowerMockito.mockStatic(SoapXMLUtils.class);
+        PowerMockito.when(SoapXMLUtils.createCoursesResultXml(USER_ID, DisplayStatus.ADMIN)).thenReturn("courseXML");
+        PowerMockito.when(SoapXMLUtils.parseCourseRefIds(foundCourses)).thenReturn(courseRefIds);
+
+        for (int courseRefId : courseRefIds) {
+            PowerMockito.when(endPointMock.getCourseXML(SID, courseRefId)).thenReturn("Course " + courseRefId +  " XML");
+            PowerMockito.when(SoapXMLUtils.createsFromCourseNodeInfo(Mockito.eq(courseRefId), Mockito.anyString())).thenReturn(new IliasNode(courseRefId, IliasNode.Type.COURSE, "Course " + courseRefId));
+        }
+
+        // == go
+        List<IliasNode> coursesForUser = classUnderTest.getCoursesForUser(DisplayStatus.ADMIN);
+
+        // == verify
+        assertEquals(3, coursesForUser.size());
+        Mockito.verify(endPointMock).getCoursesForUser(Mockito.eq(SID), Mockito.anyString());
+        PowerMockito.verifyStatic(Mockito.times(1));
+        SoapXMLUtils.createCoursesResultXml(USER_ID, DisplayStatus.ADMIN);
+        PowerMockito.verifyStatic(Mockito.times(1));
+        SoapXMLUtils.parseCourseRefIds(foundCourses);
+        Mockito.verify(endPointMock, Mockito.times(courseRefIds.size())).getCourseXML(Mockito.eq(SID), Mockito.anyInt());
+        PowerMockito.verifyStatic(Mockito.times(courseRefIds.size()));
+        SoapXMLUtils.createsFromCourseNodeInfo(Mockito.anyInt(), Mockito.anyString());
+    }
+
 }
